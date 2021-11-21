f6f0a4e39cb14117d41c1512b3df7906eab70796
Iryna Puchkova <irynapuchkova18@gmail.com>
2021-11-21 19:07:58 +0200

    fix props in modal

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -5,34 +5,32 @@ import { Overlay, ModalWrapper } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export default function Modal({onClose, children}) {
+export default function Modal({ onClose, children }) {
   useEffect(() => {
-    const handleKeydown = (e) => {
-        if (e.code === 'Escape') {
-          onClose();
-        }
-    }
-    window.addEventListener('keydown', handleKeydown)
-    return (() =>
-    window.removeEventListener('keydown', handleKeydown)
-     )
-  }, [onClose] )
-    
+    const handleKeydown = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', handleKeydown);
+    return () => window.removeEventListener('keydown', handleKeydown);
+  }, [onClose]);
+
   const handleBackdrop = e => {
     if (e.currentTarget === e.target) {
       onClose();
     }
-  }
+  };
 
-    return (createPortal(
-      <Overlay onClick={handleBackdrop}>
-        <ModalWrapper>{children}</ModalWrapper>
-      </Overlay>,
-      modalRoot,
-    ));
+  return createPortal(
+    <Overlay onClick={handleBackdrop}>
+      <ModalWrapper>{children}</ModalWrapper>
+    </Overlay>,
+    modalRoot,
+  );
 }
 
 Modal.propTypes = {
-  onClose: PropTypes.func.isRequired,
-  children: PropTypes.string.isRequired,
+  onClose: PropTypes.func,
+  // children: PropTypes.string,
 };
